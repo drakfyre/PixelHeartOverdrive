629c8bc1d3d34170a1ecc438684be0e7e5c4b2cc
Max Maroe <drakfyre@gmail.com>
2026-05-22 19:20:56 -0700

    Filenames now include track numbers, and some layout changes

--- music-player.js
+++ music-player.js
@@ -1,6 +1,6 @@
+const songAlbum = document.getElementById("song-album");
 const songImage = document.getElementById("song-image");
 const songName = document.getElementById("song-name");
-const songArtist = document.getElementById("song-artist");
 
 const songSlider = document.getElementById("slider-song");
 
@@ -25,92 +25,74 @@ const audioPlayer = document.getElementById("audio-player");
 const songs = [
     {
         name: "Example of the Hero",
-        artist: "HappyJazzyDragon",
         start: 0
     },
     {
         name: "Breaking out onto the Scene",
-        artist: "HappyJazzyDragon",
         start: 325.3
     },
     {
         name: "Hard Goin' Platformin'",
-        artist: "HappyJazzyDragon",
         start: 560.2
     },
     {
         name: "We may yet make it After All",
-        artist: "HappyJazzyDragon",
         start: 793.9
     },
     {
         name: "Aliased Horizon",
-        artist: "HappyJazzyDragon",
         start: 977.5
     },
     {
         name: "Saturated Swing",
-        artist: "HappyJazzyDragon",
         start: 1246.6
     },
     {
         name: "The Rival returns",
-        artist: "HappyJazzyDragon",
         start: 1449.5
     },
     {
         name: "Metal Arctic Zone",
-        artist: "HappyJazzyDragon",
         start: 1678
     },
     {
         name: "Drive by Wire",
-        artist: "HappyJazzyDragon",
         start: 1883.53
     },
     {
         name: "Breakneck Approach",
-        artist: "HappyJazzyDragon",
         start: 2141.4
     },
     {
         name: "Prismatic Funk",
-        artist: "HappyJazzyDragon",
         start: 2318.5
     },
     {
         name: "Happiness is a Superpower",
-        artist: "HappyJazzyDragon",
         start: 2572
     },
     {
         name: "Nostalgenosis",
-        artist: "HappyJazzyDragon",
         start: 2779.5
     },
     {
         name: "Pixel Heart Overdrive",
-        artist: "HappyJazzyDragon",
         start: 2993
     },
     {
         name: "The Rescued becomes the Rescuer",
-        artist: "HappyJazzyDragon",
         start: 3261.7
     },
     {
         name: "Big Bad ain't so Bad",
-        artist: "HappyJazzyDragon",
         start: 3491.7
     },
     {
         name: "Anthemic Epilogue",
-        artist: "HappyJazzyDragon",
         start: 3706
     },
     {
         name: "(our) Players of the Evening",
-        artist: "HappyJazzyDragon",
         start: 3907.5
     }
 ];
@@ -140,8 +122,8 @@ function loadTrack(index) {
 
     const song = songs[index];
 
+    songAlbum.textContent = "Pixel Heart Overdrive";
     songName.textContent = song.name;
-    songArtist.textContent = song.artist;
 
     if (song.image) {
         songImage.src = song.image;
